a2bda0bb03aae0b26b40f9f0ac15c2a0f328f152
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-02-12 22:35:19 +0300

    Staretd working on js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
+function DisplayErrorMessage() {
+  if (error) {
+  } else {
+    alert("Valid email required");
+  }
+}
+
+const error = document.querySelector(".btn-subscribe");
+error.addEventListener("click", DisplayErrorMessage);
+
 /*Thanks for subscribing! A confirmation email has been sent to
       ash@loremcompany.com. Please open it and click the button inside to
       confirm your subscription. Dismiss message*/
