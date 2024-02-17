21f4b9f528266c6007ddc7b9fee06032f69cfa7b
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-02-17 23:31:38 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 function handleSubscription(e) {
   e.preventDefault();
   const emailInput = document.getElementById("email");
+  console.log("Email input:", emailInput);
   const emailAddress = emailInput.value.trim();
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (emailAddress.length > 0 && emailPattern.test(emailAddress)) {
     //if successful subscription then confirmation
     alert("Successful subscription");
     const confirmation = document.getElementById("confirmation-container");
+    const subscribe = document.getElementById("subscribe-container");
     confirmation.style.display = "block";
+    subscribe.style.display = "none";
     emailInput.classList.remove("invalid-email");
     const errorMessage = document.querySelector(".error-message");
     errorMessage.style.display = "none";
@@ -16,11 +19,11 @@ function handleSubscription(e) {
     alert("Subscription failed");
     emailInput.classList.add("invalid-email");
     const errorMessage = document.querySelector(".error-message");
-    errorMessage.style.display = "inline-block";
+    errorMessage.style.display = "block";
   }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   const subscription = document.querySelector(".submit-form");
-  subscription.addEventListener("click", handleSubscription);
+  subscription.addEventListener("submit", handleSubscription);
 });
