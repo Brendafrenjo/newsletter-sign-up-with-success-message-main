3b557c7ded6894ad5fdd07938e6c6f0dbf2da7ed
Brendafrenjo <121930220+Brendafrenjo@users.noreply.github.com>
2024-02-20 19:22:53 +0300

    Added handle Dismiss
    Added the ja code for handle submit to ensure once the user click the dismiss button, they are taken back to the subscription page

--- src/app.js
+++ src/app.js
@@ -8,12 +8,15 @@ function handleSubscription(event) {
     //if successful subscription then confirmation
     const confirmation = document.getElementById("confirmation-container");
     const subscribe = document.getElementById("subscribe-container");
+    const emailValue = document.querySelector("email-value");
     confirmation.style.display = "block";
     subscribe.style.display = "none";
     emailInput.classList.remove("invalid-email");
     errorMessage.style.display = "none";
     emailInput.style.borderColor = "black";
     emailInput.style.color = "black";
+
+    emailValue.innerHTML = emailAddress;
   } else {
     //error
     emailInput.classList.add("invalid-email");
@@ -23,7 +26,17 @@ function handleSubscription(event) {
   }
 }
 
+function handleDismiss() {
+  const confirmation = document.getElementById("confirmation-container");
+  const subscribe = document.getElementById("subscribe-container");
+  confirmation.style.display = "none";
+  subscribe.style.display = "block";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   const subscription = document.querySelector(".submit-form");
   subscription.addEventListener("submit", handleSubscription);
 });
+
+const dismissBtn = document.querySelector(".dissmiss-button");
+dismissBtn.addEventListener("click", handleDismiss);
